0931d5633eb2bdd127434b8f725388ab14801b91
Templardo <dtemplarspirit@gmail.com>
2021-12-18 04:42:02 +0000

    Agrego script 2B David

---
+++ 1a  Gregorio.py
@@ -0,0 +1,7 @@
+#a.	(Gregorio)Crear un script para acceder al Dashboard Meraki (Always On) 
+#   el cual debe tener tres opciones:
+#   i.	 Crear en el Dashboard de Meraki una organización llamada Devnet-GroupPsi,
+#         en caso ya exista el grupo debe mostrar su el ID de la organización.
+#    ii.	 Crear una red llamada Network-Psi, en caso ya exista debe permitir borrarla.
+#    iii. API-Key: 76d52810510e74aac786e0b34dcb52c42d3fac0b, en caso no sea correcta 
+#         debe permitir ingresarla mediante el teclado.
