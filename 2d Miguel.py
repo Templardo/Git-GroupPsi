03548d9285ad1023cc104c0f7b2aa4bcb387f056
miguelreynolds <miguel.reynolds@gmail.com>
2021-12-18 07:27:50 +0000

    cambios2

--- 2d Miguel.py
+++ 2d Miguel.py
@@ -4,13 +4,32 @@
 # correos de todos los integrantes. 
 # Luego debe enviar un mensaje al Room con la ruta del contenedor en el Docker Hub.
 import requests
+import json
+import sys
+
+import requests
 
 access_token = 'ZjBkYWJhZjYtNmRhNS00NDgyLWEyYWYtNmYyZmE5MTRhMDczMDIwYmEwYzgtZmRm_P0A1_5d96674f-de50-43d7-ae6b-8071b71cb457'
 url = 'https://webexapis.com/v1/rooms'
 headers = {
     'Authorization': 'Bearer {}'.format(access_token),
-    'Content-Type': 'application/json'
+   'Content-Type': 'application/json'
 }
-params={'title': 'Devnet-GroupPsi_b'}
-res = requests.post(url, headers=headers, json=params)
-print(res.json())
+params={'max': '100'}
+res = requests.get(url, headers=headers, params=params)
+#print(res.json())
+
+sala = (json.dumps(res.json()["title"]))
+print(sala)
+if sala == "Devnet-GroupPsi_b":
+   print("Existe")
+else: 
+    print("no existe")
+    headers = {
+    'Authorization': 'Bearer {}'.format(access_token),
+    'Content-Type': 'application/json'
+    }
+    params={'title': 'Devnet-GroupPsi_b'}
+    res = requests.post(url, headers=headers, json=params)
+    print(res.json())
+
